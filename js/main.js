dec9e64aed4dadb83176bc82d623c417db1bedf2
Савитский Иван Васильевич <isavitskiy@phonexit.ru>
2018-05-31 15:31:01 +0300

    Add iife && refactor arrows

--- js/main.js
+++ js/main.js
@@ -1,103 +1,102 @@
 'use strict';
+(function () {
+  const Arrows = {
+    LEFT: 37,
+    RIGHT: 39
+  };
 
-const Arrows = {
-  LEFT: 37,
-  RIGHT: 39
-};
+  const mainCentral = document.querySelector(`.central`);
+  const greetingsScreen = document.querySelector(`#greeting`);
+  const rulesScreen = document.querySelector(`#rules`);
+  const gameOneScreen = document.querySelector(`#game-1`);
+  const gameTwoScreen = document.querySelector(`#game-2`);
+  const gameThreeScreen = document.querySelector(`#game-3`);
+  const statsScreen = document.querySelector(`#stats`);
 
-const mainCentral = document.querySelector(`.central`);
-const greetingsScreen = document.querySelector(`#greeting`);
-const rulesScreen = document.querySelector(`#rules`);
-const gameOneScreen = document.querySelector(`#game-1`);
-const gameTwoScreen = document.querySelector(`#game-2`);
-const gameThreeScreen = document.querySelector(`#game-3`);
-const statsScreen = document.querySelector(`#stats`);
+  const gameScreens = [greetingsScreen, rulesScreen, gameOneScreen, gameTwoScreen, gameThreeScreen, statsScreen];
 
-const gameScreens = [greetingsScreen, rulesScreen, gameOneScreen, gameTwoScreen, gameThreeScreen, statsScreen];
+  const screensCount = gameScreens.length;
+  let counter = 1;
 
-const screensCount = gameScreens.length;
-let counter = 1;
+  const arrowsTemplate = () => {
+    return `
+    <div class="arrows__wrap">
+      <style>
+        .arrows__wrap {
+          position: absolute;
+          top: 95px;
+          left: 50%;
+          margin-left: -56px;
+        }
+        .arrows__btn {
+          background: none;
+          border: 2px solid black;
+          padding: 5px 20px;
+        }
+      </style>
+      <button id="leftArrow" class="arrows__btn"><-</button>
+      <button id="rightArrow" class="arrows__btn">-></button>
+    </div>
+  `;
+  };
 
-const arrowTemplate = `
-  <div class="arrows__wrap">
-    <style>
-      .arrows__wrap {
-        position: absolute;
-        top: 95px;
-        left: 50%;
-        margin-left: -56px;
-      }
-      .arrows__btn {
-        background: none;
-        border: 2px solid black;
-        padding: 5px 20px;
-      }
-    </style>
-    <button id="leftArrow" class="arrows__btn"><-</button>
-    <button id="rightArrow" class="arrows__btn">-></button>
-  </div>
-`;
+  const createElement = (template) => {
+    const outer = document.createElement(`div`);
+    outer.innerHTML = template.innerHTML;
+    return outer;
+  };
 
-const getElementFromTemplate = (content) => {
-  let innerContent = document.createRange().createContextualFragment(content);
-  let node = document.createElement(`div`);
-  node.appendChild(innerContent);
-  let clone = node.cloneNode(true);
-  return clone;
-};
+  const changeView = (view) => {
+    mainCentral.innerHTML = ``;
+    mainCentral.appendChild(view);
+  };
 
-const createElement = (template) => {
-  const outer = document.createElement(`div`);
-  outer.innerHTML = template.innerHTML;
-  return outer;
-};
+  const showScreen = (screen) => {
+    const newElement = createElement(screen);
+    changeView(newElement);
+  };
 
-const changeView = (view) => {
-  mainCentral.innerHTML = ``;
-  mainCentral.appendChild(view);
-};
-
-const showScreen = (screen) => {
-  const newElement = createElement(screen);
-  changeView(newElement);
-};
-
-const showNextScreen = () => {
-  if (screensCount !== counter) {
-    showScreen(gameScreens[counter]);
-    counter++;
-  }
-};
-
-const showPrevScreen = () => {
-  counter--;
-  if (counter >= 0 && counter <= screensCount) {
-    if (counter === 0) {
-      counter = 1;
+  const showNextScreen = () => {
+    if (screensCount !== counter) {
+      showScreen(gameScreens[counter]);
+      counter++;
     }
-    showScreen(gameScreens[counter - 1]);
-  }
-};
+  };
 
-document.addEventListener(`keydown`, (evt) => {
-  if (counter === 0) {
-    counter++;
-  }
+  const showPrevScreen = () => {
+    counter--;
+    if (counter >= 0 && counter <= screensCount) {
+      if (counter === 0) {
+        counter = 1;
+      }
+      showScreen(gameScreens[counter - 1]);
+    }
+  };
 
-  if (evt.keyCode === Arrows.RIGHT) {
-    showNextScreen();
-  }
+  document.addEventListener(`keydown`, (evt) => {
+    if (counter === 0) {
+      counter++;
+    }
 
-  if (evt.keyCode === Arrows.LEFT) {
-    showPrevScreen();
-  }
-});
+    if (evt.keyCode === Arrows.RIGHT) {
+      showNextScreen();
+    }
 
-document.addEventListener(`DOMContentLoaded`, () => {
-  const arrowsElement = getElementFromTemplate(arrowTemplate);
-  document.body.appendChild(arrowsElement);
+    if (evt.keyCode === Arrows.LEFT) {
+      showPrevScreen();
+    }
+  });
+
+  const createArrowsElement = (template) => {
+    const outer = document.createElement(`template`);
+    outer.innerHTML = template;
+    return outer.content;
+  };
+  document.body.appendChild(createArrowsElement(arrowsTemplate()));
+
   const leftBtn = document.querySelector(`#leftArrow`);
   const rightBtn = document.querySelector(`#rightArrow`);
+
   if (leftBtn && rightBtn) {
     leftBtn.addEventListener(`click`, () => {
       showPrevScreen();
@@ -107,5 +106,6 @@ document.addEventListener(`DOMContentLoaded`, () => {
       showNextScreen();
     });
   }
+
   showScreen(greetingsScreen);
-});
+}());
