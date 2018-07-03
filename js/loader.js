ca229f12868abc577c214b2ff1a4c2b49e06d51c
Савитский Иван Васильевич <isavitskiy@phonexit.ru>
2018-07-03 23:25:25 +0300

    Prepare for submission

--- js/loader.js
+++ js/loader.js
@@ -7,7 +7,6 @@ const checkStatus = (response) => {
   if (response.status >= 200 || response.status < 300) {
     return response;
   }
-
   throw new Error(`${response.status}: ${response.statusText}`);
 };
 
